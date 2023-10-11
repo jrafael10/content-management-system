c78ef901a8ef0cc1ad0762264b724a89f9a643c6
jrafael10 <jesserafael102193@gmail.com>
2023-10-11 15:26:57 +0800

    Utilizing twig template on some of the pages

--- cms/public/index.php
+++ cms/public/index.php
@@ -2,30 +2,8 @@
 declare(strict_types  = 1);         //Use strict types
 include '../src/bootstrap.php';
 
-$articles = $cms->getArticle()->getAll(true, null, null, 6); //Get latest article summaries
+$data['articles'] = $cms->getArticle()->getAll(true, null, null, 6); //Get latest article summaries
 
-$navigation = $cms->getCategory()->getAll();            //Get categories
-$section    = '';                                      // Current category
-$title      = 'Creative Folk';                          // HTML <title> content
-$description = 'A collective of creatives for hire'; // Meta description content
-?>
-<?php include APP_ROOT . '/public/includes/header.php'; ?>
-<main class="container grid" id="content">
-    <?php foreach ($articles as $article){ ?>
-        <article class="summary">
-            <a href="article.php?id=<?= $article['id'] ?>">
-                <img src="uploads/<?= html_escape($article['image_file'] ?? 'blank.png') ?>" alt="<?= html_escape($article['image_alt']) ?>">
-                <h2><?= html_escape($article['title']) ?></h2>
-                <p><?= html_escape($article['summary']) ?></p>
-            </a>
-            <p class="credit">
-                Posted in <a href="category.php?id=<?= $article['category_id'] ?>"> <?= html_escape($article['category']) ?></a>
-                by <a href="member.php?id=<?= $article['member_id']  ?>">
-                    <?= html_escape($article['author']) ?></a>
-            </p>
-        </article>
-    <?php } ?>
-</main>
+$data['navigation'] = $cms->getCategory()->getAll();            //Get categories
 
-<?php include APP_ROOT .'/public/includes/footer.php' ?>
-
+echo $twig->render('index.html', $data);
--- cms/src/bootstrap.php
+++ cms/src/bootstrap.php
@@ -4,9 +4,6 @@ require APP_ROOT . '/src/functions.php';        //Functions
 require APP_ROOT . '/config/config.php';        //Configuration data
 require APP_ROOT . '/vendor/autoload.php';
 
-
-
-
 if(DEV === false) {
     set_exception_handler('handle_exception');
     set_error_handler('handle_error');
