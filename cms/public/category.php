e29cce87718141684f6f25476036b3d47dc4b18a
jrafael10 <jesserafael102193@gmail.com>
2023-10-15 23:06:25 +0800

    converting PHP files to Twig templates

--- cms/public/category.php
+++ cms/public/category.php
@@ -13,41 +13,15 @@ if(!$category) {                                                //If category is
 }
 
 
-$articles = $cms->getArticle()->getAll(true, $id);                 //Get articles
-$navigation = $cms->getCategory()->getAll();                      //Get navigation categories
-$section = $category['id'];                                     //Current category
-$title = $category['name'];                                     //HTML <title> content
-$description = $category['description'];                        //Meta description content
+$data['articles'] = $cms->getArticle()->getAll(true, $id);                 //Get articles
+$data['navigation'] = $cms->getCategory()->getAll();                      //Get navigation categories
+$data['section'] = $category['id'];                                     //Current category
+$data['category'] = $category;
 
-?>
+echo $twig->render('category.html', $data);                 //Render template
 
-<?php include 'includes/header.php'; ?>
-<main class="container" id="content">
-    <section class="header">
-        <h1><?= html_escape($category['name']) ?></h1>
-        <p><?= html_escape($category['description']) ?></p>
-    </section>
-    <section class="grid">
-        <?php foreach ($articles as $article) {?>
-        <article class="summary">
-            <a href="article.php?id=<?= $article['id'] ?>">
-                <img src="uploads/<?= html_escape($article['image_file'] ?? 'blank.png') ?>"
-                    alt="<?= html_escape($article['title']) ?>">
-                <h2><?= html_escape($article['title']) ?></h2>
-                <P><?=html_escape($article['summary']) ?></P>
-            </a>
-            <p class="credit">
-                Posted in <a href="category.php?id=<?= $article['category_id'] ?>">
-                    <?= html_escape($article['category']) ?></a>
-                by <a href="member.php?id=<?= $article['member_id'] ?>">
-                    <?= html_escape($article['author']) ?></a>
-            </p>
-        </article>
-        <?php } ?>
-    </section>
-</main>
-<?php
-include 'includes/footer.php' ?>
+
+
 
 
 
--- cms/public/page-not-found.php
+++ cms/public/page-not-found.php
@@ -3,20 +3,13 @@ declare(strict_types = 1);                                    // Use strict type
 http_response_code(404);                                      // Set HTTP response code
 require_once '../src/bootstrap.php';
 
-//get the navigation here from the CMS container
-$navigation = $cms->getCategory()->getAll();
-$section = '';                                              //Current category
-$title = 'Page not found';                                  //HTML <title> content
-$description = 'Page not found';                            // Meta description content
-?>
-<?php include APP_ROOT .  '/public/includes/header.php';?>
-<main class="container" id="content">
-    <h1>Sorry! We cannot find that page.</h1>
-    <p>Try the <a href="index.php">home page</a> or email us
-    <a href="mailto:hello@eg.link">hello@eg.link</a></p>
-</main>
-<?php include APP_ROOT . '/public/includes/footer.php'; ?>
-<?php exit ?>
+
+$data['navigation'] = $cms->getCategory()->getAll();          //Get categories
+
+echo $twig->render('page-not-found.html', $data);       //Render template
+
+exit;
+
 
 
 
